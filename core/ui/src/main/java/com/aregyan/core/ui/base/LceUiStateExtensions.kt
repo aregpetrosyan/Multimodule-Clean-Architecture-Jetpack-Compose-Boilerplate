b27c4b27d699f0806980ec26f6cc4b13638d5aab
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2025-09-18 16:03:16 +0200

    Removed not used code

--- core/ui/src/main/java/com/aregyan/core/ui/base/LceUiStateExtensions.kt
+++ core/ui/src/main/java/com/aregyan/core/ui/base/LceUiStateExtensions.kt
@@ -1,25 +1,5 @@
 package com.aregyan.core.ui.base
 
-import kotlinx.coroutines.flow.MutableStateFlow
-
-/** Extension helpers for MutableStateFlow of LceUiState **/
-
-fun <T> MutableStateFlow<LceUiState<T>>.setIdle() {
-    value = LceUiState.idle()
-}
-
-fun <T> MutableStateFlow<LceUiState<T>>.setLoading() {
-    value = LceUiState.loading()
-}
-
-fun <T> MutableStateFlow<LceUiState<T>>.setSuccess(data: T) {
-    value = LceUiState.success(data)
-}
-
-fun <T> MutableStateFlow<LceUiState<T>>.setError(throwable: Throwable) {
-    value = LceUiState.error(throwable)
-}
-
 /** Safely update the data only if the state is Success */
 inline fun <T> LceUiState<T>.updateSuccess(
     transform: (T) -> T
